0d26e9c604449b210b2a2479b9faef682c8b785d
jin10086 <igaojin@qq.com>
2019-12-31 00:49:07 +0800

    go

--- run.py
+++ run.py
@@ -4,8 +4,19 @@ from spider.genesis_mining import parsedata as rungenesis_mining
 from spider.miningzoo import parsedata as runminingzoo
 from spider.oxbtc import parsedata as runoxbtc
 from spider.viabtc import parsedata as runviabtc
+from spider.wayi import parsedata as runwayi
+from spider.iqmining import parsedata as runiqmining
 
-runlist = [runbitdeer, runbtccom, rungenesis_mining, runminingzoo, runoxbtc, runviabtc]
+runlist = [
+    runbitdeer,
+    runbtccom,
+    rungenesis_mining,
+    runminingzoo,
+    runoxbtc,
+    runviabtc,
+    runwayi,
+    runiqmining,
+]
 
 for app in runlist:
     app()
